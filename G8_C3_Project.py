ff999108dd091e29e4d7561089d66c566576ee4c
byjus21 <98193895+byjus21@users.noreply.github.com>
2022-01-21 17:30:31 -0600

    Add files via upload

---
+++ G8_C3_Project.py
@@ -0,0 +1,16 @@
+import turtle
+
+longitud = 10
+#Comenzar la variable longitud
+
+while length <= :#Escribe la condición
+    for i in range(10):
+        #Escribe la línea para pen color
+        #Escribe la línea para mover turtle hacia adeleante usando longitud como variable
+        turtle.right(89)
+        longitud += 0.5
+        #escribe la declaración break para salir de este bucle for
+    #Escribe esta línea para pen color
+    #Escribe esta línea para mover turtle hacia adelante usando length como variable
+    turtle.right(89)
+    longitud += 0.5
